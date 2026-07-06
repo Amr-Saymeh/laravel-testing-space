b7b0dafe8ac19ed7a408d7d3ae8282cd153c2e47
Amr-Saymeh <s12323561@stu.najah.edu>
2026-07-06 08:33:58 +0300

    Auth middleware implementation

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,26 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\BorrowController;
 use App\Http\Controllers\UserController;
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{$id}', [UserController::class, 'show']);
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{$id}', [BookController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::post('/books', [BookController::class, 'store']);
-Route::post('/borrow', [BorrowController::class, 'borrow_book']);
-Route::post('/return', [BorrowController::class, 'return_book']);
+Route::post('/signup', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{$id}', [UserController::class, 'show']);
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{$id}', [BookController::class, 'show']);
+    Route::post('/books', [BookController::class, 'store']);
+    Route::post('/borrow', [BorrowController::class, 'borrow_book']);
+    Route::post('/return', [BorrowController::class, 'return_book']);
+});
+
 
 
 
